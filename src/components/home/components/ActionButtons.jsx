41ec57ec3390a6ebb8fa5f81b0bbeca5b5d0ee31
limbersMay <limbermay1@gmail.com>
2022-11-02 17:37:35 -0600

    se envió los colores de los botones y sus colores de hover

--- src/components/home/components/ActionButtons.jsx
+++ src/components/home/components/ActionButtons.jsx
@@ -4,15 +4,24 @@ import { ButtonsContainer } from "../styled-components/ButtonsContainer"
 export const ActionButtons = () => {
     return (
         <ButtonsContainer>
-            <Button>
+            <Button 
+                background='#2E7D32'
+                hoverColor='#2a692d;'
+            >
                 Save
             </Button>
 
-            <Button>
+            <Button 
+                background='#00695C'
+                hoverColor='#005a4f;'
+                >
                 Reopen
             </Button>
 
-            <Button>
+            <Button 
+                background='#C62828'
+                hoverColor='#b71c1c;'
+                >
                 Delete
             </Button>
         </ButtonsContainer>
