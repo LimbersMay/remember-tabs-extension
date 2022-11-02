d92db70b196a780f496cecd14be74652f4ad4840
limbersMay <limbermay1@gmail.com>
2022-11-02 16:14:53 -0600

    se agregó el styled-componente de MainContainer

---
+++ src/styled-components/MainContainer.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+    background-color: #E8EAF6;
+    width: 300px;
+    height: 280px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+`;
