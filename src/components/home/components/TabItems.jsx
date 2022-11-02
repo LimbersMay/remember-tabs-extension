7d222193508a3054a2e5de29dc0d4275b1a3e09b
limbersMay <limbermay1@gmail.com>
2022-11-02 17:15:38 -0600

    se agregó el cascaron del componente de TabItems

---
+++ src/components/home/components/TabItems.jsx
@@ -0,0 +1,18 @@
+import { TabItemsContainer } from "../styled-components/TabItemsContainer"
+import { TabItem } from "./TabItem"
+
+
+export const TabItems = () => {
+    return (
+        <TabItemsContainer>
+
+            <TabItem />
+            <TabItem />
+            <TabItem />
+            <TabItem />
+            <TabItem />
+            <TabItem />
+
+        </TabItemsContainer>
+    )
+}
