475eaa102d916824b8f6a8e997d72d8c2ced9e0e
limbersMay <limbermay1@gmail.com>
2022-11-02 17:32:50 -0600

    se estiló el botón y se agregó un color de manera condicional

--- src/components/home/styled-components/Button.jsx
+++ src/components/home/styled-components/Button.jsx
@@ -1,7 +1,14 @@
 import styled from 'styled-components';
 
 export const Button = styled.button`
-    background: white;
+
+    background-color: ${ props => props.background || "gray"};
+    
+    color: white;
+    border-style: none;
+    border-radius: 5px;
     font-weight: bold;
+    width: 75px;
     padding: 4px;
+    min-height: 35px;
 `;
