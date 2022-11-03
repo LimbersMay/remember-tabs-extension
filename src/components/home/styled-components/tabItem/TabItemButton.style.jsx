fb7d5a50036cd436ab76b7cac45b9c53b8beb415
limbersMay <limbermay1@gmail.com>
2022-11-02 22:05:19 -0600

    se estiló el botón del tabItem

--- src/components/home/styled-components/tabItem/TabItemButton.style.jsx
+++ src/components/home/styled-components/tabItem/TabItemButton.style.jsx
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 
 export const TabItemButton = styled.button`
+    background-color: ${ props => props.background || 'white' };
+    width: 80%;
     color: white;
+    border-style: none;
+    border-radius: 4px;
+
+    :hover {
+        background-color: ${ props => props.hover || 'white' };
+    }
 `;
