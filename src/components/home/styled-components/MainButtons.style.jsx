15902006a0d24ad1cf6a2ce7caa3014dc78babf9
limbersMay <limbermay1@gmail.com>
2022-11-02 22:01:50 -0600

    se quitó la coma adicional del estilo

--- src/components/home/styled-components/MainButtons.style.jsx
+++ src/components/home/styled-components/MainButtons.style.jsx
@@ -5,7 +5,7 @@ export const MainButtons = styled.div`
     display: flex;
     justify-content: space-around;
     align-items: center;
-    background-color: #E8EAF6;;
+    background-color: #E8EAF6;
     width: 100%;
     height: 50px;
 `;
