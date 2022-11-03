9fbbc0f3715ed458badd88bcd03bd025b09e10d3
limbersMay <limbermay1@gmail.com>
2022-11-02 21:04:13 -0600

    se creó un archivo de barril para exportar todos los componentes estilizados

--- src/components/home/styled-components/index.js
+++ src/components/home/styled-components/index.js
@@ -2,15 +2,15 @@
 export * from './Header.style';
 export * from './Title.style';
 
-export * from './TabItems.style';
-export * from './TabItem.style';
+export * from './tabItem/TabItems.style';
+export * from './tabItem/TabItem.style';
 
-export * from './TabItemInfo.style';
-export * from './TabItemButtons.style';
-export * from './TabItemButton.style';
+export * from './tabItem/TabItemInfo.style';
+export * from './tabItem/TabItemButtons.style';
+export * from './tabItem/TabItemButton.style';
 
-export * from './TabItemTitle.style';
-export * from './TabItemUrl.style';
+export * from './tabItem/TabItemTitle.style';
+export * from './tabItem/TabItemUrl.style';
 
 export * from './MainButtons.style';
 export * from './Button.style';
