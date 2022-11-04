ce0812e8d442748b25b94745ca0c838cc60bb736
limbersMay <limbermay1@gmail.com>
2022-11-03 20:30:25 -0600

    se agregó los eventos de los componentes para guardar, eliminar y abrir las tabs

--- src/components/home/components/MainButtons.jsx
+++ src/components/home/components/MainButtons.jsx
@@ -1,25 +1,33 @@
+import { useContext } from 'react';
 import * as S from '../styled-components';
+import { TabContext } from './context/TabContext';
 
 export const MainButtons = () => {
+
+    const { handleSetTabs, handleOpenTabs, handleDeleteTabs } = useContext(TabContext);
+
     return (
         <S.MainButtons>
             <S.MainButton 
                 background='#2E7D32'
-                hoverColor='#2a692d;'
+                hoverColor='#2a692d'
+                onClick={ handleSetTabs }
             >
                 Save
             </S.MainButton>
 
             <S.MainButton 
                 background='#00695C'
-                hoverColor='#005a4f;'
+                hoverColor='#005a4f'
+                onClick={ handleOpenTabs }
                 >
                 Reopen
             </S.MainButton>
 
             <S.MainButton 
                 background='#C62828'
-                hoverColor='#b71c1c;'
+                hoverColor='#b71c1c'
+                onClick={ handleDeleteTabs }
                 >
                 Delete
             </S.MainButton>
