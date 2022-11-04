452aae1dddb47088bfa80805d2b74fee1fb30694
limbersMay <limbermay1@gmail.com>
2022-11-03 21:41:58 -0600

    se controló el desbordamiento del titulo

--- src/components/home/styled-components/tabItem/TabItemTitle.style.jsx
+++ src/components/home/styled-components/tabItem/TabItemTitle.style.jsx
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 
 export const TabItemTitle = styled.h4`
-    font-size: 15px;
+    font-size: 17px;
     margin: 0px;
+    min-height: 10px;
+
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
 `;
