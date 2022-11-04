d064d0b10d0cb276446c5c9e4ad88a4e1e65ea11
limbersMay <limbermay1@gmail.com>
2022-11-03 18:21:08 -0600

    se movió a un solo lugar la lógica de los botones para reducir la complejidad

--- src/components/home/components/TabItemButtons.jsx
+++ src/components/home/components/TabItemButtons.jsx
@@ -1,28 +1,33 @@
+import { useContext } from "react";
 import * as S from "../styled-components";
+import { TabContext } from "./context/TabContext";
 import { TabItemButton } from "./TabItemButton";
 
 
-export const TabItemButtons = () => {
+export const TabItemButtons = ({ tab }) => {
+
+    const { handleSetTabs } = useContext(TabContext);
+
     return (
         <S.TabItemButtons>
+            
+            <S.TabItemButton
+                background={'#00695C'}
+                hover={'#005a4f'}
 
-            <TabItemButton
-                text={'Go'}
+                onClick={ () => handleSetTabs(tab.id) }
+            >
+                Go
+            </S.TabItemButton>
 
-                styles={{
-                    background: '#00695C',
-                    hover: '#005a4f'
-                }}
-            />
+            <S.TabItemButton
+                background={'#C62828'}
+                hover={'#b71c1c'}
 
-            <TabItemButton
-                text={'Delete'}
-                
-                styles={{
-                    background: '#C62828',
-                    hover: '#b71c1c'
-                }}
-            />
+                onClick={ () => handleSetTabs(tab.id) }
+            >
+                Delete
+            </S.TabItemButton>
 
         </S.TabItemButtons>
     )
