597cf23faa578e5ac6d155dd3ecb132b1290fad3
limbersMay <limbermay1@gmail.com>
2022-11-03 18:21:53 -0600

    se renderizaron de manera condicional los cardTabs

--- src/components/home/components/TabItems.jsx
+++ src/components/home/components/TabItems.jsx
@@ -1,13 +1,29 @@
 import * as S from "../styled-components";
 import { TabItem } from "./TabItem";
 
+import { v4 } from 'uuid'
+import { useContext } from "react";
+import { TabContext } from "./context/TabContext";
 
 export const TabItems = () => {
+
+    const { tabs, isLoading } = useContext(TabContext);
+
     return (
         <S.TabItems>
 
-            <TabItem />
-            <TabItem /> 
+            {
+                isLoading
+                    
+                &&
+
+                tabs.map( tab => (
+                    <TabItem 
+                        key={v4()}
+                        tab={tab}
+                    />
+                ))            
+            }
             
         </S.TabItems>
     )
