de9286c9d7f59f5c91fcadbced91d7fcbb0930d4
limbersMay <limbermay1@gmail.com>
2022-11-04 22:05:37 -0600

    se agregaron los componentes para animar los items

--- src/components/home/components/TabItems.jsx
+++ src/components/home/components/TabItems.jsx
@@ -2,25 +2,33 @@ import * as S from "../styled-components";
 import { TabItem } from "./TabItem";
 
 import { v4 } from 'uuid'
-import { useContext } from "react";
+import { createRef, useContext, useRef } from "react";
 import { TabContext } from "./context/TabContext";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 export const TabItems = () => {
 
     const { tabsState } = useContext(TabContext);
 
     return (
-        <S.TabItems>
 
+        <TransitionGroup component={S.TabItems} appear={true}>
             {
-                tabsState.map( tab => (
-                    <TabItem 
-                        key={v4()}
-                        tab={tab}
-                    />
-                ))            
+                tabsState.map((tab, index) => (
+                    <CSSTransition
+                        key={tab.id}
+                        timeout={300}
+                        classNames="transition"
+                    >
+
+                        <TabItem 
+                            key={ v4() }
+                            tab={ tab }
+                        ></TabItem>
+
+                    </CSSTransition>
+                ))
             }
-            
-        </S.TabItems>
+        </TransitionGroup>
     )
 }
