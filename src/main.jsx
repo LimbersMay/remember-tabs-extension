9400e6da9238f15598f09157f445f29e82993871
limbersMay <limbermay1@gmail.com>
2022-12-04 09:31:45 -0600

    se renderizó el componente de aplicación2

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { TabProvider } from './components/home/components/context/TabProvider';
-import { HomeApp } from './components/home/HomeApp'
+import { App } from './App';
 
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <TabProvider>
-            <HomeApp />
-        </TabProvider>
+        <App />
     </React.StrictMode>
 )
