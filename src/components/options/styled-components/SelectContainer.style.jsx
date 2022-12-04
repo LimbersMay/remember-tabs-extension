8390f53862f47fe75e1c8bffa53b9381dd69e7fe
limbersMay <limbermay1@gmail.com>
2022-12-04 14:42:59 -0600

    se estiló el contenedor del select

---
+++ src/components/options/styled-components/SelectContainer.style.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const SelectContainer = styled.div`
+    display: flex;   
+    flex-direction: row;
+    justify-content: space-evenly;
+    padding-top: 15px;
+`
