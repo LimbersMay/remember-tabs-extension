f69d25e64b693c38450d25bfba0178c3c289bf30
limbersMay <limbermay1@gmail.com>
2022-12-04 14:36:01 -0600

    se agregó el MemoryRouter en lugar de browserRouter para la extensión de chrome

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import { TabProvider } from './components/home/components/context/TabProvider';
-import { HomeApp } from './components/home/HomeApp';
-import { MainLayout } from './layouts/MainLayout';
+import {TabProvider} from './components/home/components/context/TabProvider';
+import {HomeApp} from './components/home/HomeApp';
+import {MainLayout} from './layouts/MainLayout';
+
+import {MemoryRouter, Route, Routes} from 'react-router-dom';
+import {OptionsApp} from "./components/options/OptionsApp";
+
 
 export const App = () => {
 
     return (
-        <TabProvider>
+        <MemoryRouter >
+            <TabProvider>
                 <MainLayout>
-                    <HomeApp/>
+
+                    <Routes>
+                        <Route path='/' element={<HomeApp/>}/>
+                        <Route path='/options-extended' element={<OptionsApp/>}/>
+                    </Routes>
+
                 </MainLayout>
-        </TabProvider>
+            </TabProvider>
+        </MemoryRouter>
     )
 }
