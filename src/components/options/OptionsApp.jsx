c6f10ffcf6597d1f84151829c26102f8b90d73fa
limbersMay <limbermay1@gmail.com>
2022-12-06 07:41:58 -0600

    se renombró la importación de OptionButtons

--- src/components/options/OptionsApp.jsx
+++ src/components/options/OptionsApp.jsx
@@ -1,11 +1,11 @@
 import {PreferencesContainer} from "./components/PreferencesContainer.jsx";
-import {ButtonsContainer} from "./components/ButtonsContainer.jsx";
+import {OptionButtons} from "./components/OptionButtons.jsx";
 
 export const OptionsApp = () => {
     return (
         <>
           <PreferencesContainer/>
-          <ButtonsContainer/>
+          <OptionButtons/>
         </>
     )
 }
