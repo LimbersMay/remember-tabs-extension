4966c963ee7693c6ac242a6634124c545828badc
limbersMay <limbermay1@gmail.com>
2022-12-06 07:44:21 -0600

    se exportó los estilos de Button y ButtonsContainer

--- src/components/options/styled-components/index.js
+++ src/components/options/styled-components/index.js
@@ -2,3 +2,5 @@
 export * from './Select.style.jsx';
 export * from './SelectContainer.style.jsx';
 export * from './PreferencesContainer.style.jsx';
+export * from './Button.style.jsx';
+export * from './ButtonsContainer.style.jsx';
