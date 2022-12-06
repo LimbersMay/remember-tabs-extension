8d3abca04dafc884d659aa02fc99aec0f245b450
limbersMay <limbermay1@gmail.com>
2022-12-06 07:40:09 -0600

    Se agregó el componente para renderizar toda la vista de opciones

--- src/components/options/OptionsApp.jsx
+++ src/components/options/OptionsApp.jsx
@@ -1,9 +1,11 @@
 import {PreferencesContainer} from "./components/PreferencesContainer.jsx";
+import {ButtonsContainer} from "./components/ButtonsContainer.jsx";
 
 export const OptionsApp = () => {
     return (
         <>
           <PreferencesContainer/>
+          <ButtonsContainer/>
         </>
     )
 }
