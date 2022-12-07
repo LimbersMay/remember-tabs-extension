2a66362c1715274a9cea51afc92b215240b3a95e
limbersMay <limbermay1@gmail.com>
2022-12-07 11:47:51 -0600

    se agregó un name al select

--- src/components/options/components/Select.jsx
+++ src/components/options/components/Select.jsx
@@ -2,17 +2,17 @@
 import { v4 as uuid } from 'uuid';
 import * as S from '../styled-components';
 
-export const Select = ({selected, options}) => {
+export const Select = ({selected, options, name}) => {
 
     return (
         <S.SelectContainer>
-            <S.Select>
+            <S.Select name={name}>
                 {
                     options.map( element => (
                          <option
                              key={uuid()}
                              value={ element.key }
-                             defaultChecked={selected === element.key}
+                             selected={selected === element.key}
                          >
                              { element.value }
                          </option>
