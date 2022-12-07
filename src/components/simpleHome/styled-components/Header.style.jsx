960aa3a87ca1b1c38e43ea91d1e07bbe12f9ac22
limbersMay <limbermay1@gmail.com>
2022-12-07 13:21:59 -0600

    Bold y color blanco agregado al header

--- src/components/simpleHome/styled-components/Header.style.jsx
+++ src/components/simpleHome/styled-components/Header.style.jsx
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 
-export const Header = styled`
+export const Header = styled.header`
   background-color: #B71C1C;
   width: 100%;
   display: flex;
   justify-content: center;
+  color: white;
+  font-weight: bold;
 `
