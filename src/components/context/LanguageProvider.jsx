ef5e418ca9258a593201695145dd4137de1e9add
limbersMay <limbermay1@gmail.com>
2022-12-06 21:35:45 -0600

    Diccionarios de inglés y español agregados

--- src/components/context/LanguageProvider.jsx
+++ src/components/context/LanguageProvider.jsx
@@ -1,49 +1,78 @@
 import {LanguageContext} from "./LanguageContext";
 
-const spanishDictionary = {
-    openButtonCard: 'Abrir',
-    openButton: 'Abrir todo',
-    deleteButton: "Borrar",
-    saveButton: "Guardar",
-    optionsButton: "Opciones",
-    placeholderSelectLanguage: "Seleccionar idioma",
-    placeholderSelectLayout: "Seleccionar diseño",
-    cancelButton: "Cancelar",
-    selectLanguage: {
-        spanish: "Español",
-        english: "Inglés"
+const dictionaries = {
+    'ES-MX': {
+        openButtonCard: 'Abrir',
+        openButton: 'Abrir todo',
+        deleteButton: "Borrar",
+        saveButton: "Guardar",
+        optionsButton: "Opciones",
+        placeholderSelectLanguage: "Seleccionar idioma",
+        placeholderSelectLayout: "Seleccionar diseño",
+        cancelButton: "Cancelar",
+        selectLanguage: [
+            {
+                value: "Español",
+                key: "ES-MX"
+            },
+            {
+                value: "English",
+                key: "EN-US"
+            }
+        ],
+        selectLayout: [
+            {
+                value: "Extendido",
+                key: "EX"
+            },
+            {
+                value: "Simple",
+                key: "SIM"
+            }
+        ]
+    },
+    'EN-US': {
+        openButtonCard: 'Open',
+        openButton: 'Reopen',
+        deleteButton: "Delete",
+        saveButton: "Save",
+        optionsButton: "Options",
+        placeholderSelectLanguage: "Select language",
+        placeholderSelectLayout: "Select layout",
+        cancelButton: "Cancel",
+        selectLanguage: [
+            {
+                value: "Spanish",
+                key: "ES-MX"
+            },
+            {
+                value: "English",
+                key: "EN-US"
+            }
+        ],
+        selectLayout: [
+            {
+                value: "Extended",
+                key: "EX"
+            },
+            {
+                value: "Simple",
+                key: "SIM"
+            }
+        ]
     }
 }
 
-const englishDictionary = {
-    openButtonCard: 'Open',
-    openButton: 'Reopen',
-    deleteButton: "Delete",
-    saveButton: "Save",
-    optionsButton: "Options",
-    placeholderSelectLanguage: "Select language",
-    placeholderSelectLayout: "Select layout",
-    cancelButton: "Cancel",
-    selectLanguage: {
-        spanish: "Spanish",
-        english: "English"
-    }
-}
 export const LanguageProvider = ({ children }) => {
 
-    const getSpanishDictionary = () => {
-        return spanishDictionary;
-    }
-
-    const getEnglishDictionary = () => {
-        return englishDictionary;
+    const getDictionaryByLanguage = (language) => {
+        return dictionaries[language] ?? dictionaries['EN-US'];
     }
 
     return (
         <LanguageContext.Provider value={
             {
-                getSpanishDictionary,
-                getEnglishDictionary
+                getDictionaryByLanguage
             }
         }>
             {children}
