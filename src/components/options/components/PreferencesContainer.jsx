770e96ead17ab0bf2ebd405b2894829b3e30de04
limbersMay <limbermay1@gmail.com>
2022-12-07 11:47:17 -0600

    se agregó un name a los select

--- src/components/options/components/PreferencesContainer.jsx
+++ src/components/options/components/PreferencesContainer.jsx
@@ -9,13 +9,20 @@ export const PreferencesContainer = () => {
 
     const { language, layout } = useContext( PreferencesContext );
     const { getUSerDictionary } = useContext( LanguageContext );
-
     const {selectLanguage, selectLayout} = getUSerDictionary();
 
     return (
         <S.PreferencesContainer>
-            <Select  options={ selectLanguage } selected={ language }/>
-            <Select  options={ selectLayout } selected={ layout } />
+            <Select
+                options={ selectLanguage }
+                selected={ language }
+                name={ "language" }
+            />
+            <Select
+                options={ selectLayout }
+                selected={ layout }
+                name={ "layout" }
+            />
         </S.PreferencesContainer>
     )
 }
