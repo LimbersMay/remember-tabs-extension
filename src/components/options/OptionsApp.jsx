3e92223870b0b7efab0536ad06996c4faa0c8f76
limbersMay <limbermay1@gmail.com>
2022-12-07 11:37:35 -0600

    Método onSubmit del formulario agregado

--- src/components/options/OptionsApp.jsx
+++ src/components/options/OptionsApp.jsx
@@ -1,11 +1,24 @@
 import {PreferencesContainer} from "./components/PreferencesContainer.jsx";
 import {OptionButtons} from "./components/OptionButtons.jsx";
+import {useContext} from "react";
+import {PreferencesContext} from "../context/PreferencesContext.jsx";
+import {useNavigate} from "react-router-dom";
 
 export const OptionsApp = () => {
+
+    const { handleSetLanguage, handleSetLayout } = useContext(PreferencesContext);
+    const history = useNavigate();
+    const onSubmit = (event) => {
+        event.preventDefault();
+        handleSetLanguage(event.target[0].value);
+        handleSetLayout(event.target[1].value);
+        history('/');
+    }
+
     return (
-        <>
-          <PreferencesContainer/>
-          <OptionButtons/>
-        </>
+        <form onSubmit={onSubmit}>
+            <PreferencesContainer/>
+            <OptionButtons/>
+        </form>
     )
 }
