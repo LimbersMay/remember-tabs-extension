a30a902fde74781c4606bab3740f59cfdd0bee8e
limbersMay <limbermay1@gmail.com>
2022-12-07 13:11:35 -0600

    Se estiló el contendor del layout simple2

---
+++ src/styled-components/SimpleMainLayout.style.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const SimpleMainLayout = styled.div`
+  background-color: #E8EAF6;
+  width: 150px;
+  height: 280px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
