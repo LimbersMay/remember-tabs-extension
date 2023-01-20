1714e830b4de72af92df5bb7850c35f36900b7be
limberMay <limbermay1@gmail.com>
2023-01-20 12:01:48 -0600

    (store) cascaron del thunk tabs creado

---
+++ src/store/rememberTabs/tab/thunks.js
@@ -0,0 +1,18 @@
+
+export const startLoadingTabs = () => {
+    return async(dispatch) => {
+
+    }
+}
+
+export const startSaveTabs = () => {
+    return async(dispatch) => {
+
+    }
+}
+
+export const startDeleteTabs = () => {
+    return async(dispatch) => {
+
+    }
+}
