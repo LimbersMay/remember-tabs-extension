405c58d60843210505f0ce1cc6b1372d8855592c
limberMay <limbermay1@gmail.com>
2023-01-20 17:42:58 -0600

    (TabItems) tabs del usuario seleccionados desde el store de redux

--- src/home/components/TabItems.jsx
+++ src/home/components/TabItems.jsx
@@ -1,20 +1,18 @@
 import * as S from "../styled-components";
 import { TabItem } from "./TabItem";
-
-import { useContext } from "react";
-import { TabContext } from "./context/TabContext";
 import { TransitionGroup } from "react-transition-group";
 import { TransitionItem } from "./TransitionItem";
+import {useSelector} from "react-redux";
 
 export const TabItems = () => {
 
-    const { tabsState } = useContext(TabContext);
+    const { tabs } = useSelector(state => state.tabs)
 
     return (
 
         <TransitionGroup component={S.TabItems}>
             {
-                tabsState.map(tab => (
+                tabs.map(tab => (
                     <TransitionItem key={tab.id}>
                         <TabItem tab={tab} />
                     </TransitionItem>
