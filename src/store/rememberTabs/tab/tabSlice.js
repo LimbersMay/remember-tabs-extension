8cdae63c54de97aec96e97b84c85d711c549e294
limberMay <limbermay1@gmail.com>
2023-01-20 14:48:28 -0600

    (store/tab) reducers de deleteTabs y deleteTabById agregados al slice

--- src/store/rememberTabs/tab/tabSlice.js
+++ src/store/rememberTabs/tab/tabSlice.js
@@ -9,9 +9,15 @@ export const tabSlice = createSlice({
         setTabs: (state, action) => {
             state.tabs = action.payload;
         },
+        deleteTabs: (state) => {
+            state.tabs = []
+        },
+        deleteTabById: (state, action) => {
+            state.tabs = state.tabs.filter(tab => tab.id !== action.payload);
+        }
     }
 });
 
 
 // Action creators are generated for each case reducer function
-export const {setTabs} = tabSlice.actions;
+export const { setTabs, deleteTabs, deleteTabById } = tabSlice.actions;
