4d1cdc8601364dd147eeb40627ef39d510843508
limberMay <limbermay1@gmail.com>
2023-01-20 17:34:22 -0600

    (root) App refactorizado, se devolvió solo el <AppRouter />

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,14 @@
-import {MemoryRouter, Route, Routes} from 'react-router-dom';
-
-import {OptionsApp} from "./options/OptionsApp";
-import {HomeApp} from './home/HomeApp';
-import {SimpleHomeApp} from "./simpleHome/SimpleHomeApp";
-
-import {LanguageProvider, PreferencesProvider} from "./context";
-
-import {MainLayout} from './layouts';
-import {SimpleMainLayout} from "./layouts";
-
-import {ConditionalRoute} from "./components/ConditionalRoute";
-import {TabProvider} from "./home/components/context/TabProvider.jsx";
-
+import {MemoryRouter} from 'react-router-dom';
+import {LanguageProvider} from "./context";
+import {AppRouter} from "./routes/AppRouter";
 
 export const App = () => {
 
     return (
-        <MemoryRouter >
-            <TabProvider>
-                <PreferencesProvider>
-                    <LanguageProvider>
-
-                        <Routes>
-                            <Route element={<MainLayout/>}>
-                                <Route path='/home-extended' element={<HomeApp />} />
-                                <Route path='/options-extended' element={<OptionsApp/>}/>
-                            </Route>
-
-                            <Route path='/' element={<ConditionalRoute />}/>
-
-                            <Route element={<SimpleMainLayout/>}>
-                                <Route path='/simple-home' element={<SimpleHomeApp/>}/>
-                                <Route path='/simple-options' element={<OptionsApp/>}/>
-                            </Route>
-                        </Routes>
-
-                    </LanguageProvider>
-                </PreferencesProvider>
-            </TabProvider>
+        <MemoryRouter>
+            <LanguageProvider>
+                <AppRouter/>
+            </LanguageProvider>
         </MemoryRouter>
     )
 }
