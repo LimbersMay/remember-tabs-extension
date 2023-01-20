d0ffd64ca076af50e0452ae418b412015ee7d483
limberMay <limbermay1@gmail.com>
2023-01-20 12:01:32 -0600

    (store) cascaron del thunk preferences creado

---
+++ src/store/rememberTabs/preferences/thunks.js
@@ -0,0 +1,35 @@
+export const startLoadingLanguage = () => {
+    return async(dispatch) => {
+
+        // async code here
+
+        // sync code here
+
+    }
+}
+
+export const startLoadingLayout = () => {
+    return async(dispatch) => {
+
+        // async code here
+
+        // sync code here
+
+    }
+}
+
+export const startSaveLanguage = () => {
+    return async(dispatch) => {
+        // async code here
+
+        // sync code here
+    }
+}
+
+export const startSaveLayout = () => {
+    return async(dispatch) => {
+        // async code here
+
+        // sync code here
+    }
+}
