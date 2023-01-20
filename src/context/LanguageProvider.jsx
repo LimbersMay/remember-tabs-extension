b5c9fd5d7d200e715d4d4acee393f9d7996906e8
limberMay <limbermay1@gmail.com>
2023-01-20 17:43:43 -0600

    (TabItemsButtons) Se obtuvo el idioma preferido desde el store usando useSelector

--- src/context/LanguageProvider.jsx
+++ src/context/LanguageProvider.jsx
@@ -1,93 +1,16 @@
 import {LanguageContext} from "./LanguageContext.jsx";
-import {useContext} from "react";
-import {PreferencesContext} from "./PreferencesContext.jsx";
-
-const dictionaries = {
-    'ES-MX': {
-        openButtonCard: 'Abrir',
-        openButton: 'Abrir',
-        deleteButton: "Borrar",
-        saveButton: "Guardar",
-        optionsButton: "Opciones",
-        placeholderSelectLanguage: "Seleccionar idioma",
-        placeholderSelectLayout: "Seleccionar diseño",
-        cancelButton: "Cancelar",
-        selectLanguage: [
-            {
-                value: "Español",
-                key: "ES-MX"
-            },
-            {
-                value: "Inglés",
-                key: "EN-US"
-            }
-        ],
-        selectLayout: [
-            {
-                value: "Extendido",
-                key: "EXTENDED"
-            },
-            {
-                value: "Simple",
-                key: "SIMPLE"
-            }
-        ],
-        simplesaveButton: "Guardar Tabs",
-        simpleOpenButton: "Reabrir Tabs",
-        simpleDeleteButton: "Borrar Tabs",
-        simpleSucessMessage: "Tabs guardados",
-        simpleReopenTabsMessage: "Tabs abiertos",
-        simpleDeleteTabsMessage: "Tabs borrados",
-    },
-    'EN-US': {
-        openButtonCard: 'Open',
-        openButton: 'Reopen',
-        deleteButton: "Delete",
-        saveButton: "Save",
-        optionsButton: "Options",
-        placeholderSelectLanguage: "Select language",
-        placeholderSelectLayout: "Select layout",
-        cancelButton: "Cancel",
-        selectLanguage: [
-            {
-                value: "Spanish",
-                key: "ES-MX"
-            },
-            {
-                value: "English",
-                key: "EN-US"
-            }
-        ],
-        selectLayout: [
-            {
-                value: "Extended",
-                key: "EXTENDED"
-            },
-            {
-                value: "Simple",
-                key: "SIMPLE"
-            }
-        ],
-        simplesaveButton: "Save current Tabs",
-        simpleOpenButton: "Reopen Tabs",
-        simpleDeleteButton: "Delete Tabs",
-        simpleSucessMessage: "Tabs saved",
-        simpleReopenTabsMessage: "Tabs opened",
-        simpleDeleteTabsMessage: "Tabs deleted",
-    }
-}
+import {useSelector} from "react-redux";
+import dictionaries from "../languages/languages.js";
 
 export const LanguageProvider = ({ children }) => {
 
-    const { language } = useContext(PreferencesContext);
-    const getUSerDictionary = () => {
-        return dictionaries[language] ?? dictionaries['EN-US'];
-    }
+    const { language } = useSelector(state => state.preferences);
+    const userDictionary = dictionaries[language] ?? dictionaries['EN-US'];
 
     return (
         <LanguageContext.Provider value={
             {
-                getUSerDictionary
+                userDictionary
             }
         }>
             {children}
