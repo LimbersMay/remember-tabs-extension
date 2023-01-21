14da498768866b0bca8ac146b0ffa3efae81a4b9
limberMay <limbermay1@gmail.com>
2023-01-20 19:25:32 -0600

    Se arregló el problema de que las propiedadades de layout y language no existan en el localStorage, en ese caso, no se hace ningún cambio

--- src/store/rememberTabs/preferences/preferencesSlice.js
+++ src/store/rememberTabs/preferences/preferencesSlice.js
@@ -4,14 +4,24 @@ export const preferencesSlice = createSlice({
     name: 'preferences',
     initialState: {
         language: 'EN-US',
-        layout: null,
+        layout: 'EXTENDED',
         checking: 'checking'
     },
     reducers: {
         setLanguage: (state, action) => {
+
+            if (!action.payload) {
+                return state;
+            }
+
             state.language = action.payload;
         },
         setLayout: (state, action) => {
+
+            if (!action.payload) {
+                return state;
+            }
+
             state.layout = action.payload;
         },
         setChecking: (state, action) => {
