dc0403c829b1c1383f21d217e609c50520713da5
LimbersMay <limbermay1@gmail.com>
2024-05-17 17:24:24 -0600

    Add UserDictionary interface

---
+++ src/interfaces/UserDictionary.ts
@@ -0,0 +1,28 @@
+interface LanguageOptions {
+    value: string;
+    key: string;
+}
+
+interface LayoutOptions {
+    value: string;
+    key: string;
+}
+
+export interface UserDictionary {
+    openButtonCard: string;
+    openButton: string;
+    deleteButton: string;
+    saveButton: string;
+    optionsButton: string;
+    placeholderSelectLanguage: string;
+    placeholderSelectLayout: string;
+    cancelButton: string;
+    languageOptions: LanguageOptions[],
+    layoutOptions: LayoutOptions[],
+    simplesaveButton: string;
+    simpleOpenButton: string;
+    simpleDeleteButton: string;
+    simpleSucessMessage: string;
+    simpleReopenTabsMessage: string;
+    simpleDeleteTabsMessage: string;
+}
