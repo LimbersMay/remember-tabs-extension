3b4fdcdf47fc7bc3fd419189163d7cc99d1eb40c
LimbersMay <limbermay1@gmail.com>
2024-05-17 20:23:16 -0600

    Header resize issue fixed

--- src/home/components/Header.tsx
+++ src/home/components/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
     return (
-        <div className="bg-[#006064] w-full flex h-[50px] justify-center items-center">
+        <div className="bg-[#006064] w-full flex h-[50px] min-h-[50px] justify-center items-center">
             <h4 className="font-bold text-white text-[20px]">
                 RememberTabs
             </h4>
