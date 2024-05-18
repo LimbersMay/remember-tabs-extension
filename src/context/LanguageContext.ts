00068851506ed5f1e25c91d0755b49e23ffe92f8
LimbersMay <limbermay1@gmail.com>
2024-05-17 18:55:09 -0600

    Refactor import paths and use useAppSelector in LanguageProvider.tsx

--- src/context/LanguageContext.ts
+++ src/context/LanguageContext.ts
@@ -1,8 +1,11 @@
 import {createContext} from "react";
 import {UserDictionary} from "../interfaces/UserDictionary";
+import languages from "../languages/languages.ts";
 
 interface LanguageContextProps {
     userDictionary: UserDictionary;
 }
 
-export const LanguageContext = createContext<LanguageContextProps>(null);
+export const LanguageContext = createContext<LanguageContextProps>({
+    userDictionary: languages['EN-US']
+});
--- src/context/LanguageProvider.tsx
+++ src/context/LanguageProvider.tsx
@@ -2,8 +2,9 @@ import {LanguageContext} from "./LanguageContext";
 import dictionaries from "../languages/languages";
 import {useAppSelector} from "../store/hooks";
 import {SelectPreferences} from "../store/rememberTabs";
+import {ReactNode} from "react";
 
-export const LanguageProvider = ({ children }) => {
+export const LanguageProvider = ({ children }: { children: ReactNode }) => {
 
     const { language } = useAppSelector(SelectPreferences);
     const userDictionary = dictionaries[language] ?? dictionaries['EN-US'];
