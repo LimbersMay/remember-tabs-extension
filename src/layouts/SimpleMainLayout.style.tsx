a77c3f5a23c6125474f7444e594a8ddce0858264
LimbersMay <limbermay1@gmail.com>
2024-05-17 18:37:33 -0600

    Refactor import paths and add type annotation to SimpleMainLayout component

--- src/layouts/SimpleMainLayout.style.tsx
+++ src/layouts/SimpleMainLayout.style.tsx
@@ -1,7 +1,8 @@
 import {Outlet} from "react-router-dom";
 import {Header} from "../simpleHome/components/Header";
+import {ReactNode} from "react";
 
-export const SimpleMainLayout = ({ children }) => {
+export const SimpleMainLayout = ({ children }: { children: ReactNode }) => {
     return (
         <div className="flex flex-col items-center justify-start bg-gray-100 w-[150px] h-[350px]">
 
