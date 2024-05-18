67bec158772eb38944d6cbbc253a082a6669d872
LimbersMay <limbermay1@gmail.com>
2024-05-17 18:37:11 -0600

    Refactor import paths and add type annotation to TabItem component

--- src/home/components/TabItem.tsx
+++ src/home/components/TabItem.tsx
@@ -1,7 +1,8 @@
 import { TabItemButtons } from "./TabItemButtons";
 import { TabItemInfo } from "./TabItemInfo";
+import {Tab} from "../../interfaces/Tab.ts";
 
-export const TabItem = ({ tab }) => {
+export const TabItem = ({ tab }: { tab: Tab }) => {
 
 	return (
 		<div className="flex items-center justify-between bg-[#d7d9e6] m-[10px] w-[95%] rounded-[5px] min-h-[75px] hover:bg-[#c7c9d6]">
