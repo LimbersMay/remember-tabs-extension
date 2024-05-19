dc85baf2a01762c25c77e45fb44de6672c9fa8e4
LimbersMay <limbermay1@gmail.com>
2024-05-18 19:18:00 -0600

    Refactor import paths and use AppDispatch type in FooterButton.tsx

--- src/home/components/FooterButton.tsx
+++ src/home/components/FooterButton.tsx
@@ -1,4 +1,10 @@
-export const FooterButton = ({ onClick, appendClass, value }) => {
+interface FooterButtonProps {
+    onClick: () => void;
+    appendClass: string;
+    value: string;
+}
+
+export const FooterButton = ({ onClick, appendClass, value }: FooterButtonProps) => {
     return (
         <button
             className={`text-white border-none rounded-[5px] font-bold min-w-[70px] max-w-[90px] p-[4px] min-h-[35px] cursor-pointer ${appendClass}`}
