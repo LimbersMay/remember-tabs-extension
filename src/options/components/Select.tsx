bb8a3ad48748f04d2573d751d2c7740269d4e9cd
LimbersMay <limbermay1@gmail.com>
2024-11-06 20:49:41 -0600

    Label agregado a todos los selects para mejorar la experiencia del usuario

--- src/options/components/Select.tsx
+++ src/options/components/Select.tsx
@@ -6,28 +6,40 @@ interface SelectProps {
     options: LanguageOptions[]
     name: string;
     additionalClasses?: string;
+    label?: string;
 }
 
-export const Select = ({ selected, options, name, additionalClasses }: SelectProps) => {
+export const Select = ({ selected, options, name, additionalClasses, label }: SelectProps) => {
 
     return (
-        <div className="flex w-full flex-row justify-evenly">
-            <select
-                name={name}
-                className={`outline-none rounded-[5px] h-[35px] w-[80%] p-[5px] bg-[#d7d9e6] ${additionalClasses}`}
-                defaultValue={selected}
+        <div className="w-full">
+            <label
+                className="flex flex-row w-full items-center justify-between pl-12 pr-10"
+                htmlFor={name}
             >
-                {
-                    options.map(element => (
-                        <option
-                            key={uuid()}
-                            value={element.key}
-                        >
-                            {element.value}
-                        </option>
-                    ))
-                }
-            </select>
+                <span className="font-bold text-tabText">
+                    {
+                        label ? label : name.charAt(0).toUpperCase() + name.slice(1)
+                    }
+                </span>
+                <select
+                    name={name}
+                    className={`outline-none rounded-[5px] h-[35px] w-[60%] p-[5px] bg-[#d7d9e6] ${additionalClasses}`}
+                    defaultValue={selected}
+                >
+                    <option value="" disabled> Select {name} </option>
+                    {
+                        options.map(element => (
+                            <option
+                                key={uuid()}
+                                value={element.key}
+                            >
+                                {element.value}
+                            </option>
+                        ))
+                    }
+                </select>
+            </label>
         </div>
     );
 };
