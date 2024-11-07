28a1c2eccb8617df8fc9f7126c4ddae971d953ff
LimbersMay <limbermay1@gmail.com>
2024-11-06 20:48:59 -0600

    Se eliminó el apartado de donación con kofi y se agregó la opción de autoguardado de las pestañas

--- src/options/components/PreferencesContainer.tsx
+++ src/options/components/PreferencesContainer.tsx
@@ -3,14 +3,13 @@ import { Select } from "./Select";
 import { LanguageContext } from "../../context";
 import {useAppSelector} from "../../store/hooks.ts";
 import {SelectPreferences} from "../../store/rememberTabs";
-import kofiLogo from "../../../public/assets/kofi.png";
 
 export const PreferencesContainer = () => {
 
-    const { language, layout, theme } = useAppSelector(SelectPreferences);
+    const { language, layout, theme, autoSave } = useAppSelector(SelectPreferences);
 
     const { userDictionary } = useContext(LanguageContext);
-    const {languageOptions, layoutOptions, themeOptions} = userDictionary;
+    const {languageOptions, layoutOptions, themeOptions, autoSaveOptions } = userDictionary;
 
     return (
         <div className="flex flex-col items-center gap-[10px] mt-[25px] mb-[20px]">
@@ -31,18 +30,11 @@ export const PreferencesContainer = () => {
                 name={"theme"}
             />
 
-            <a
-                href="https://ko-fi.com/limbersdev"
-                target="_blank"
-                className="flex items-center w-[80%] h-[35px] border-none rounded-[5px] p-[5px] text-white bg-[#13C3FF] justify-center hover:shadow cursor-pointer"
-            >
-                <img
-                    src={kofiLogo}
-                    alt="Kofi Logo"
-                    className="w-[25px] h-[25px] inline-block"
-                />
-                Support me on Kofi
-            </a>
+            <Select
+                options={autoSaveOptions}
+                selected={autoSave}
+                name={"autoSave"}
+            />
         </div>
     )
 }
